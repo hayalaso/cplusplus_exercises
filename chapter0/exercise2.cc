af11592829ceee9d3e13c23b672adf1f753d1004
Hugo Ayala <hgchavo05@gmail.com>
2019-11-30 14:11:15 -0500

    adding first exercises

---
+++ chapter0/exercise2.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+  std::cout<<"\tThis (\") is a quote, and this (\\) is a backslash"<<std::endl;
+}
---
+++ chapter0/hello.cc
@@ -0,0 +1,10 @@
+// small C++ program
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  cout<<"Hello,\nworld!"<<endl;
+  return 0;
+}
